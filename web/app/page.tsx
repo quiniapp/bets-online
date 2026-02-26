9c290ffebbf59c788e5a89390a85cb9c6c833cc2
quiniapp <quini.app.leo@gmail.com>
2026-02-26 08:47:09 -0300

    feat: render under construction page on landing route

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,22 +1,5 @@
-import { Flex, FlexCol } from "@/components/flex";
-import Box from "@/components/box";
- 
-import HeaderIndex from "@/components/header";
-import HeroBannerIndex from "@/feature/hero";
-import Footer from "@/components/footer";
-import GamesList from "@/components/games";
- 
+import UnderConstructionPage from "@/feature/under-construction"
+
 export default function LandingPage() {
-  return (
-    <Box className="grid grid-rows-[auto_1fr_auto] h-full">
-      <HeaderIndex />
-      <FlexCol className="items-center">
-        <HeroBannerIndex />
-        <Flex className="p-4 border-t-1 justify-center h-full">
-          <GamesList />
-        </Flex>
-      </FlexCol>
-      <Footer />
-    </Box>
-    )
+  return <UnderConstructionPage />
 }
